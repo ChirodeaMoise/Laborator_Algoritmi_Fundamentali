928033dc1f139c853b55950ba0d26aec33d4efff
Chirodea Moise <tiii3139@gmail.com>
2021-04-07 11:34:54 +0300

    add Fiboncci Sort_myVersion

--- Fibonacci Search_myVersion/Fibonacci Search_myVersion/Program.cs
+++ Fibonacci Search_myVersion/Fibonacci Search_myVersion/Program.cs
@@ -9,7 +9,8 @@ namespace Fibonacci_Search_myVersion
             float a = 0, b = 1, x, i;
             Console.WriteLine("Introduceti Pozitia: ");
             p =int.Parse(Console.ReadLine());
-            int[] v = new int[p]; 
+            int[] v = { 1,2,3,4 };
+           // int[] v = new int[p];
             if (a == 0 && b == 1)
             {
                 for (i = 0; i < v.Length; i++)
